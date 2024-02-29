b160fcd9d09f8527379855e5f2635e427efbcd6a
Michael Crawford <cerealean@users.noreply.github.com>
2024-02-29 02:51:47 +0000

    Updated return type to be a boolean

--- src/startsWith/startsWith.mjs
+++ src/startsWith/startsWith.mjs
@@ -2,7 +2,7 @@
  * 
  * @param {string} str The string to evaluate
  * @param {string} searchString Case sensitive string to search for within the given string
- * @returns 
+ * @returns {boolean}
  */
 export function startsWith(str, searchString) {
     return str.indexOf(searchString) === 0;
